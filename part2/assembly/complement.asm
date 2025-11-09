cdf5315ff66a6af2c107205709076f3c4d0702c6
DS <divyam23@iiserb.ac.in>
2025-11-09 15:00:27 +0530

    before starting sunday's work

--- part2/assembly/complement.asm
+++ part2/assembly/complement.asm
@@ -1,25 +1,18 @@
-;logical AND, OR and XOR operations
-;output displayed using 7447 IC
+;program to complement a boolean
 
-.include "/home/ubuntu/DigitalWorkshop/part2/assembly/m328Pdef.inc"
+ 
+.include "/home/gadepall/m328Pdef.inc"
 
 ldi r16, 0b00111100 ;identifying output pins 2,3,4,5
 out DDRD,r16		;declaring pins as output
 
+ldi r16,0b00000000	;A=0
 
-
-ldi r16,0b00000010	;initializing W
-ldi r17,0b00000100	;initializing X
-;logical AND
-;and r16,r17		;W AND X
-;logical OR
-or r16,r17			;W OR X
-;logical XOR
-;eor r16,r17			;X XOR X
+rcall comp	;jumpting to comp routine below
 
 ;following code is for displaying output
 ;shifting LSB in r16 to 2nd position
-ldi r20, 0b00000010	
+ldi r20, 0b00000010	;counter = 2
 
 rcall loopw		;calling the loopw routine
 
@@ -34,3 +27,13 @@ loopw:	lsl r16			;left shift
 	dec r20			;counter --
 	brne	loopw	;if counter != 0
 	ret
+
+;comp routine begins
+comp:
+	mov r0,r16			;using r0 for computations
+	ldi r16,0b00000001	;loading 1
+	
+	eor r16,r0			;A'=A XOR 1
+	ret 				;returning from comp
+
+
